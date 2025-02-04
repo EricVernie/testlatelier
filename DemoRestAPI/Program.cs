e9b28182e8bc97cf6ed44acdc4f3d1401b4b14be
Eric Vernié <eric.vernie@hotmail.fr>
2025-02-04 19:20:43 +0100

    add task 2 et 3

--- DemoRestAPI/Program.cs
+++ DemoRestAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,15 +23,75 @@ if (app.Environment.IsDevelopment())
 
 //Crée un endpoint qui retourne les joueurs. La liste doit être triée du meilleur au moins bon.
 
+//TODO : AJouter attribut authorisation
 app.MapGet("/Players", async (ServicePlayers servicePlayers ) =>
 {
-    var Players = await servicePlayers.GetPlayerAsync();
-    
-
-    return Results.Ok(Players.players.OrderBy (p => p.data.rank));
+    var RootObject = await servicePlayers.GetPlayersAsync();
+    return Results.Ok(RootObject.players.OrderBy (p => p.data.rank));
     
 })
 .WithName("GetJoueurs")
+.WithOpenApi();
+
+app.MapGet("/Players/{id}", async (string id, ServicePlayers servicePlayers) =>
+{
+    var player = await servicePlayers.GetPlayerByIdAsync(id);
+    if (player == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(player);
+})
+.WithName("GetPlayerById")
+.WithOpenApi();
+
+// Crée un endpoint qui retourne les statistiques des joueurs.
+app.MapGet("/Players/Statistics", async (ServicePlayers servicePlayers) =>
+{
+    var RootObject = await servicePlayers.GetPlayersAsync();
+
+    // Pays avec le plus grand ratio de parties gagnées
+    var countryWithBestWinRatio = RootObject.players
+        .GroupBy(p => p.country.code)
+        .Select(g => new
+        {
+            Code=g.Select(p => p.country.code).First(),
+            WinRatio = g.Sum(p => p.data.points) 
+        })
+        .OrderByDescending(g => g.WinRatio)
+        .FirstOrDefault();
+
+    // IMC moyen de tous les joueurs
+    var averageBMI = RootObject.players
+        .Select(p => p.data.weight / (p.data.height * p.data.height))
+        .Average();
+
+    // Médiane de la taille des joueurs
+    var medianHeight = RootObject.players
+        .Select(p => p.data.height)
+        .OrderBy(h => h)
+        .ToList();
+    double median;
+    int count = medianHeight.Count;
+    if (count % 2 == 0)
+    {
+        median = (medianHeight[count / 2 - 1] + medianHeight[count / 2]) / 2;
+    }
+    else
+    {
+        median = medianHeight[count / 2];
+    }
+
+    var statistics = new
+    {
+        CountryWithBestWinRatio = countryWithBestWinRatio,
+        AverageBMI = averageBMI,
+        MedianHeight = median
+    };
+
+    return Results.Ok(statistics);
+})
+.WithName("GetPlayerStatistics")
 .WithOpenApi();
 
 //app.UseHttpsRedirection();
--- DemoRestAPI/Services/ServicePlayers.cs
+++ DemoRestAPI/Services/ServicePlayers.cs
@@ -4,24 +4,44 @@ public class ServicePlayers
 {
     private readonly HttpClient _httpClient;
     private readonly IConfiguration _configuration;
+    private Rootobject _rootobject;
+
     public ServicePlayers(HttpClient httpClient, IConfiguration configuration)
     {
         _httpClient = httpClient;
         _configuration = configuration;
     }   
     
-    public async Task<Rootobject> GetPlayerAsync()
+    public async Task<Rootobject> GetPlayersAsync()
     {
         //TODO : Add try catch
         //TODO : Add logging
         //TODO : Add caching
         //TODO : Test null values
+        if (_rootobject != null)
+        {
+            return _rootobject;
+        }
+        return _rootobject = await internalGetPlayersAsync();
 
+    }
+    public async Task<Player> GetPlayerByIdAsync(string id)
+    {
+        if (_rootobject == null)
+        {
+            _rootobject = await internalGetPlayersAsync();
+        }
+        Player? player=_rootobject.players.Find(p => p.id == Convert.ToInt32(id));
+        return player;
+    }
+    private async Task<Rootobject> internalGetPlayersAsync()
+    {
         var HeadToHeadUrl = _configuration["headtoheadurl"];
         var Response = await _httpClient.GetAsync(HeadToHeadUrl);
         Response.EnsureSuccessStatusCode();
         var JsonContent = await Response.Content.ReadAsStringAsync();
-        var Players = JsonSerializer.Deserialize<Rootobject>(JsonContent);
-        return Players;
+        _rootobject = JsonSerializer.Deserialize<Rootobject>(JsonContent);
+        return _rootobject;
     }
+
 }
